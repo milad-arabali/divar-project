93b546c5f1ffcaa40db7eeac25f923787d73ecdb
milad <miladarabali1@gmail.com>
2025-07-14 15:38:13 +0330

    1-dr #: create project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+    return (
+        <>
+            <p className="bg-yellow-400 text-black font-Vazir p-4">مثال</p>
+        </>
+    )
+}
+
+export default App
